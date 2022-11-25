494fc20562c52edb021dde5a4664b99e7144453b
hy0000 <2877079184@qq.com>
2022-11-25 15:17:52 +0800

    update noc interface definition

--- src/main/scala/NoC/NocInterface.scala
+++ src/main/scala/NoC/NocInterface.scala
@@ -4,18 +4,16 @@ import spinal.core._
 import spinal.lib._
 import spinal.lib.bus.simple.PipelinedMemoryBus
 
-case class NocConfig(dataWidth: Int,
-                     regAddrWidth: Int)
+case class NocConfig(dataWidth: Int)
 
 case class NocInterface(c:NocConfig) extends Bundle with IMasterSlave {
   import c._
 
-  val regCtrl = PipelinedMemoryBus(regAddrWidth, dataWidth)
-  val dataSend = Stream(Bits(dataWidth bits))
-  val dataRec = Stream(Bits())
+  val send = Stream(Fragment(Bits(dataWidth bits)))
+  val rec = Stream(Fragment(Bits(dataWidth bits)))
 
   override def asMaster(): Unit = {
-    master(regCtrl, dataRec)
-    slave(dataSend)
+    master(send)
+    slave(rec)
   }
 }
