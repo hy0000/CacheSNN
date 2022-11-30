f02d8d92bab7c0e1c6968778feb72e5965e0b63f
hy0000 <2877079184@qq.com>
2022-11-30 15:41:13 +0800

    add test util

--- src/test/scala/CacheSNN/CacheSnnTest.scala
+++ src/test/scala/CacheSNN/CacheSnnTest.scala
@@ -2,6 +2,31 @@ package CacheSNN
 
 import spinal.core.sim._
 
+import scala.util.Random
+
 object CacheSnnTest {
   val simConfig = SpinalSimConfig(_spinalConfig = MySpinalConfig).withWave
+
+  def vToRaw(v: Seq[Int], width: Int): BigInt = {
+    val mask = (1 << width) - 1
+    v.foldRight(BigInt(0))((s, acc) => (acc << width) | (s & mask))
+  }
+
+  def rawToV(raw: BigInt, width: Int, n: Int): Seq[Int] = {
+    val mask = (1 << width) - 1
+    val msb = 1 << (width - 1)
+    val tyb = 1 << width
+    (0 until n).map(i => (raw >> (i * width) & mask).toInt)
+      .map { m => if ((m & msb) == 0) m else m - tyb }
+  }
+
+  def randomIntN(n: Int) = {
+    val upBound = 1 << (n - 1)
+    Random.nextInt(upBound * 2) - upBound
+  }
+
+  def randomInt16 = {
+    val upBound = 1 << 15
+    Random.nextInt(upBound * 2) - upBound
+  }
 }
