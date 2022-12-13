d2e1745ea6bd1278a701f746f4f9c16f969af200
hy0000 <2877079184@qq.com>
2022-12-13 15:52:35 +0800

    add SpikeManager

--- src/main/scala/Synapse/SpikeManager.scala
+++ src/main/scala/Synapse/SpikeManager.scala
@@ -3,6 +3,7 @@ package Synapse
 import spinal.core._
 import spinal.lib._
 import spinal.lib.bus.simple.PipelinedMemoryBus
+import spinal.lib.fsm.{State, StateMachine}
 
 object SpikeManager {
   val setIndexRange = log2Up(CacheConfig.lines/CacheConfig.ways)-1 downto 0
@@ -16,19 +17,32 @@ class SpikeManager extends Component {
     val spike = slave(Stream(new Spike))
     val spikeEvent = master(Stream(new SpikeEvent))
     val bus = master(MemAccessBus())
-    val dataFill = master(Stream(Fragment(new SynapseData)))
-    val dataWriteBack = master(Stream(Fragment(new SynapseData)))
+    val dataFill = master(Stream(new SynapseData))
+    val dataWriteBack = master(Stream(new SynapseData))
     val synapseEventDone = slave(Stream(new Spike))
+    val flush = slave(Event)
   }
 
-  val spikeAllocator = new SpikeCacheAllocator
+  val spikeCacheManager = new SpikeCacheManager
   val missManager = new MissSpikeManager
-  val hitQueue = StreamFifo(cloneOf(io.spikeEvent.payload), 512)
-  val readyQueue = StreamFifo(cloneOf(io.spikeEvent.payload), 4)
+  val hitQueue = StreamFifo(new SpikeEvent, 256)
+  val missQueue = StreamFifo(MissSpike(), 256)
+  val readyQueue = StreamFifo(new SpikeEvent, 4)
 
-  io.spike >> spikeAllocator.io.spikeIn
-  spikeAllocator.io.hitSpike >> hitQueue.io.push
-  spikeAllocator.io.missSpike >> missManager.io.missSpike
+  val flushLogic = new Area {
+    val inFlush = RegInit(False)
+    inFlush
+      .riseWhen(spikeCacheManager.io.free && io.flush.valid)
+      .fallWhen(spikeCacheManager.io.free && missManager.io.free)
+
+    spikeCacheManager.io.flush := inFlush
+    io.flush.ready := inFlush.fall()
+  }
+
+  io.spike.haltWhen(flushLogic.inFlush) >> spikeCacheManager.io.spikeIn
+  spikeCacheManager.io.hitSpike >> hitQueue.io.push
+  spikeCacheManager.io.missSpike >> missQueue.io.push
+  missManager.io.missSpike << missQueue.io.pop
   missManager.io.readySpike >> readyQueue.io.push
 
   io.spikeEvent <-< StreamArbiterFactory.lowerFirst
@@ -37,27 +51,62 @@ class SpikeManager extends Component {
   missManager.io.bus <> io.bus
   missManager.io.dataIn << io.dataFill
   missManager.io.dataOut >> io.dataWriteBack
-  io.synapseEventDone >> spikeAllocator.io.synapseEventDone
+  io.synapseEventDone >> spikeCacheManager.io.synapseEventDone
 }
 
-class SpikeCacheAllocator extends Component {
+case class MissSpike() extends SpikeEvent {
+  val replaceNid = cloneOf(nid)
+  val writeBackOnly = Bool()
+}
+
+class SpikeCacheManager extends Component {
   val io = new Bundle {
     val spikeIn = slave(Stream(new Spike))
-    val missSpike = master(Stream(new SpikeEvent))
+    val missSpike = master(Stream(MissSpike()))
     val hitSpike = master(Stream(new SpikeEvent))
     val synapseEventDone = slave(Stream(new Spike))
+    val flush = in Bool()
+    val free = out Bool()
   }
   stub()
 }
 
 class MissSpikeManager extends Component {
   val io = new Bundle {
-    val dataIn = slave(Stream(Fragment(new SynapseData)))
-    val dataOut = master(Stream(Fragment(new SynapseData)))
+    val dataIn = slave(Stream(new SynapseData))
+    val dataOut = master(Stream(new SynapseData)) // data out will keep ready until a full trans done
     val bus = master(MemAccessBus())
-    val missSpike = slave(Stream(new SpikeEvent))
+    val missSpike = slave(Stream(MissSpike()))
     val readySpike = master(Stream(new SpikeEvent))
-    val inferenceOnly = in Bool()
+    val free = out Bool()
   }
-  stub()
+
+  val outstanding = 2
+  val spikeBuffer = Vec(Flow(new SpikeEvent), outstanding) setAsReg()
+  spikeBuffer.foreach(_.valid init False)
+  val spikeBufferOccupancy = CountOne(spikeBuffer.map(_.valid))
+
+  io.dataIn.ready := False
+  io.dataOut.valid := False
+  io.bus.cmd.valid := False
+  io.missSpike.ready := False
+  io.readySpike.valid := False
+
+  val fsm = new StateMachine {
+    val idle = makeInstantEntry()
+    val writeBack = new State
+    val fill = new State
+
+    idle
+      .whenIsActive{
+
+      }
+      .whenIsActive{
+        goto(fill)
+      }
+    fill
+      .whenIsActive{
+
+      }
+  }
 }
