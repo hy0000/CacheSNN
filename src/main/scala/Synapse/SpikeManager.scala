23f38881aa5dcf7471f17a26255dd91f37dbfcff
hy0000 <2877079184@qq.com>
2022-12-14 10:47:46 +0800

    WIP SpikeCacheManager

--- src/main/scala/Synapse/SpikeManager.scala
+++ src/main/scala/Synapse/SpikeManager.scala
@@ -1,16 +1,9 @@
 package Synapse
 
+import CacheSNN.AER
 import spinal.core._
 import spinal.lib._
-import spinal.lib.bus.simple.PipelinedMemoryBus
-import spinal.lib.fsm.{State, StateMachine}
-
-object SpikeManager {
-  val setIndexRange = log2Up(CacheConfig.lines/CacheConfig.ways)-1 downto 0
-
-  val tagStep = CacheConfig.ways / 2
-  val tagRamAddressWidth = setIndexRange.size + log2Up(tagStep)
-}
+import spinal.lib.fsm._
 
 class SpikeManager extends Component {
   val io = new Bundle {
@@ -53,7 +46,13 @@ case class MissSpike() extends SpikeEvent {
 }
 
 class SpikeCacheManager extends Component {
+  val setIndexRange = log2Up(CacheConfig.lines / CacheConfig.ways) - 1 downto 0
+  val tagWidth = AER.nidWidth - setIndexRange.size
+  val tagTimestampWidth = 2
+  val wayCountPerStep = 2
+
   val io = new Bundle {
+    val timestamp = in UInt(tagTimestampWidth bits)
     val spikeIn = slave(Stream(new Spike))
     val missSpike = master(Stream(MissSpike()))
     val hitSpike = master(Stream(new SpikeEvent))
@@ -61,7 +60,50 @@ class SpikeCacheManager extends Component {
     val flush = slave(Event)
     val free = out Bool()
   }
-  stub()
+
+  val spikeFifo = StreamFifo(new Spike, 512)
+  val failSpike = Stream(new Spike)
+  spikeFifo.io.push << StreamArbiterFactory.lowerFirst
+    .on(Seq(failSpike, io.spikeIn))
+
+  case class TagItem() extends Bundle {
+    val valid = Bool()
+    val locked = Bool()
+    val tag = UInt(tagWidth bits)
+    val timeStamp = UInt(tagTimestampWidth bits)
+  }
+
+  // last way of per cache set is use to allocate miss spike if there are not available item to replace
+  val tagArray = Seq.fill(wayCountPerStep)(Mem(TagItem(), CacheConfig.lines / wayCountPerStep))
+  val tagReadCmd = Vec(Stream(UInt(tagArray.head.addressWidth bits)), tagArray.length)
+  val tagReadRsp = Vec(tagArray.zip(tagReadCmd).map(z => z._1.streamReadSync(z._2, z._2.payload)))
+  val tagWriteCmd = Vec(tagArray.map(_.writePort()))
+  tagReadCmd.foreach(_.setIdle())
+  tagReadRsp.foreach(_.setBlocked())
+  tagWriteCmd.foreach(_.setIdle())
+
+  val flushFsm = new StateMachine {
+    val running = new State with EntryPoint {
+      val cnt = Counter(CacheConfig.lines)
+      val tagArraySel = cnt(cnt.high downto cnt.high-log2Up(wayCountPerStep))
+      val readCmd = tagReadCmd(tagArraySel)
+      val readRsp = tagReadRsp(tagArraySel)
+      readCmd.valid := True
+      readCmd.payload := cnt.resized
+
+      io.missSpike << readRsp.translateWith{
+        val ret = cloneOf(io.missSpike.payload)
+        ret.cacheAddr := readRsp
+      }
+    }
+  }
+
+  val fsm = new StateMachine{
+    val idle = makeInstantEntry()
+    val spikeCacheAllocate = new State
+    val spikeCacheFree = new State
+    val flush = new State
+  }
 }
 
 class MissSpikeManager extends Component {
