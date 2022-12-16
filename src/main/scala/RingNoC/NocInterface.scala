8b28db738897fb8c370c2c93948030b94f5b7ea9
hy0000 <2877079184@qq.com>
2022-12-16 11:39:35 +0800

    update NocInterfaceLocal by splitting head and body

--- src/main/scala/RingNoC/NocInterface.scala
+++ src/main/scala/RingNoC/NocInterface.scala
@@ -1,6 +1,7 @@
 package RingNoC
 
 import spinal.core._
+import spinal.core.internals.BaseNode
 import spinal.lib._
 
 class NocInterface extends Bundle {
@@ -23,12 +24,30 @@ object NocInterface {
   def apply() = Stream(Fragment(new NocInterface))
 }
 
+class NocPackageHead extends Bundle {
+  val dest = UInt(4 bits)
+  val src = UInt(4 bits)
+  val custom = Bits(48 bits)
+}
+
+class NocPackage extends Bundle with IMasterSlave {
+  // behaviour is like the axi aw/w channel
+  // body should not fire earlier than head
+  val head = Stream(new NocPackageHead)
+  val body = Stream(Fragment(Bits(64 bits)))
+
+  override def asMaster(): Unit = {
+    master(head)
+    slave(body)
+  }
+}
+
 object NocInterfaceLocal{
   def apply() = new NocInterfaceLocal()
 }
 
 class NocInterfaceLocal extends Bundle with IMasterSlave {
-  val send, rec = NocInterface()
+  val send, rec = new NocPackage
 
   override def asMaster(): Unit = {
     master(send)
