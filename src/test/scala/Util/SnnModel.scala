8ab473ba5a0bb2d9abbad91df4163318680f9140
hy0000 <2877079184@qq.com>
2023-02-01 17:06:42 +0800

    fix stdpUpdate

--- src/test/scala/Util/SnnModel.scala
+++ src/test/scala/Util/SnnModel.scala
@@ -8,10 +8,10 @@ class SnnModel(preLen:Int, postLen:Int) {
     (_, _) => 1
   }
   val current = Array.fill(postLen)(0)
-  //val ltpLut = (1 to 16).toArray
-  //val ltdLut = (1 to 16).map(-_).toArray
-  val ltpLut = Array.fill(16)(1)//(1 to 16).toArray
-  val ltdLut = Array.fill(16)(0)//(1 to 16).map(-_).toArray
+  val ltpLut = (1 to 16).toArray
+  val ltdLut = (1 to 16).map(_*(-10)).toArray
+  //val ltpLut = Array.fill(16)(0)//(1 to 16).toArray
+  //val ltdLut = Array.fill(16)(0)//(1 to 16).map(-_).toArray
 
   def weightRandomize(): Unit ={
     for(i <- 0 until preLen){
@@ -38,32 +38,40 @@ class SnnModel(preLen:Int, postLen:Int) {
   }
 
   def spikeUpdate(preSpike:Array[Array[Int]], postSpike:Array[Array[Int]]): Unit = {
-    val preSpikeLastFireTime = Array.fill(preLen)(-16)
-    val postSpikeLastFireTime = Array.fill(postLen)(-16)
-    for(t <- preSpike.indices){
-      spikeForward(preSpike(t))
-      // update ltd weight
+    val preSpikeLastFireTime = Array.fill(preLen)(-222)
+    val postSpikeLastFireTime = Array.fill(postLen)(-333)
+    val epoch = preSpike.length
+    for(t <- 0 until epoch) {
+      // update weight
       for(i <- 0 until preLen) {
         if (preSpike(t)(i) == 1) {
-          preSpikeLastFireTime(i) = t
+          val ppsTime = preSpikeLastFireTime(i)
           for (j <- 0 until postLen) {
-            val deltaT = t - postSpikeLastFireTime(j)
-            if (deltaT < 16) {
-              weight(i)(j) += ltdLut(deltaT)
+            // ltp
+            if(ppsTime >= 0){
+              var ltpT = ppsTime
+              while (ltpT < t && postSpike(ltpT)(j) == 0) {
+                ltpT += 1
+              }
+              if(ltpT!=t){
+                val ltpDeltaT = ltpT - ppsTime
+                weight(i)(j) += ltpLut(ltpDeltaT)
+              }
+            }
+            // acc current
+            current(j) += weight(i)(j)
+            // ltd
+            if(postSpikeLastFireTime(j)>=ppsTime){
+              val ltdDeltaT = t - postSpikeLastFireTime(j)
+              weight(i)(j) += ltdLut(ltdDeltaT)
             }
           }
+          preSpikeLastFireTime(i) = t
         }
       }
-      // update ltp weight
-      for(j <- 0 until postLen) {
-        if(postSpike(t)(j)==1) {
-          postSpikeLastFireTime(j) = t + 1
-          for(i <- 0 until preLen) {
-            val deltaT = t - preSpikeLastFireTime(i)
-            if (deltaT < 16) {
-              weight(i)(j) += ltpLut(deltaT)
-            }
-          }
+      for(j <- 0 until postLen){
+        if(postSpike(t)(j)==1){
+          postSpikeLastFireTime(j) = t
         }
       }
     }
